e7ebda7005f8eb09d98326041d202985f8311601
Frederick Thompson <67383790+FredLeeThompson@users.noreply.github.com>
2020-09-08 16:38:32 -0500

    Updated button links. Removed 'index.js'.

--- index.js
+++
@@ -1,7 +0,0 @@
-function runButton() {
-    document.getElementById("linkButton").onclick = function () {
-      window.location.href = "https://www.linkedin.com/in/fredleethompson"
-    }
-};
-
-// runButton();
